165b5b3015831d3f3f330674b7f05df3b80e2bb8
micpe <micpe@DESKTOP-760T0BJ>
2022-07-10 16:36:55 +0200

    Dodaj pliki projektów.

---
+++ RockPaperScisors/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace RockPaperScisors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.paperbtn = new System.Windows.Forms.Button();
+            this.rockbtn = new System.Windows.Forms.Button();
+            this.scissorsbtn = new System.Windows.Forms.Button();
+            this.plaScore = new System.Windows.Forms.Label();
+            this.compScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.timGame = new System.Windows.Forms.Timer(this.components);
+            this.startbtn = new System.Windows.Forms.Button();
+            this.stopbtn = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.AutoSize = true;
+            this.player.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.player.Location = new System.Drawing.Point(134, 366);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(160, 55);
+            this.player.TabIndex = 5;
+            this.player.Text = "Player";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(632, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 55);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Computer";
+            // 
+            // picBox2
+            // 
+            this.picBox2.Image = global::RockPaperScisors.Properties.Resources.x;
+            this.picBox2.Location = new System.Drawing.Point(642, 440);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(225, 225);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 8;
+            this.picBox2.TabStop = false;
+            // 
+            // picBox1
+            // 
+            this.picBox1.Image = global::RockPaperScisors.Properties.Resources.x;
+            this.picBox1.Location = new System.Drawing.Point(104, 440);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(225, 225);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 7;
+            this.picBox1.TabStop = false;
+            // 
+            // paperbtn
+            // 
+            this.paperbtn.Image = global::RockPaperScisors.Properties.Resources._1Paper;
+            this.paperbtn.Location = new System.Drawing.Point(642, 12);
+            this.paperbtn.Name = "paperbtn";
+            this.paperbtn.Size = new System.Drawing.Size(282, 283);
+            this.paperbtn.TabIndex = 4;
+            this.paperbtn.UseVisualStyleBackColor = true;
+            this.paperbtn.Click += new System.EventHandler(this.paperbtn_Click);
+            // 
+            // rockbtn
+            // 
+            this.rockbtn.Image = global::RockPaperScisors.Properties.Resources._2Rock;
+            this.rockbtn.Location = new System.Drawing.Point(12, 12);
+            this.rockbtn.Name = "rockbtn";
+            this.rockbtn.Size = new System.Drawing.Size(282, 283);
+            this.rockbtn.TabIndex = 3;
+            this.rockbtn.UseVisualStyleBackColor = true;
+            this.rockbtn.Click += new System.EventHandler(this.rockbtn_Click);
+            // 
+            // scissorsbtn
+            // 
+            this.scissorsbtn.Image = global::RockPaperScisors.Properties.Resources.Scissors;
+            this.scissorsbtn.Location = new System.Drawing.Point(327, 12);
+            this.scissorsbtn.Name = "scissorsbtn";
+            this.scissorsbtn.Size = new System.Drawing.Size(282, 283);
+            this.scissorsbtn.TabIndex = 1;
+            this.scissorsbtn.UseVisualStyleBackColor = true;
+            this.scissorsbtn.Click += new System.EventHandler(this.scissorsbtn_Click);
+            // 
+            // plaScore
+            // 
+            this.plaScore.AutoSize = true;
+            this.plaScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.plaScore.Location = new System.Drawing.Point(385, 512);
+            this.plaScore.Name = "plaScore";
+            this.plaScore.Size = new System.Drawing.Size(51, 55);
+            this.plaScore.TabIndex = 9;
+            this.plaScore.Text = "0";
+            // 
+            // compScore
+            // 
+            this.compScore.AutoSize = true;
+            this.compScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.compScore.Location = new System.Drawing.Point(539, 512);
+            this.compScore.Name = "compScore";
+            this.compScore.Size = new System.Drawing.Size(51, 55);
+            this.compScore.TabIndex = 10;
+            this.compScore.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(469, 512);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 55);
+            this.label2.TabIndex = 11;
+            this.label2.Text = ":";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.resetBtn.Location = new System.Drawing.Point(615, 746);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(282, 64);
+            this.resetBtn.TabIndex = 12;
+            this.resetBtn.Text = "Reset ";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // timGame
+            // 
+            this.timGame.Tick += new System.EventHandler(this.timGame_Tick);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startbtn.Location = new System.Drawing.Point(39, 746);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(282, 64);
+            this.startbtn.TabIndex = 13;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // stopbtn
+            // 
+            this.stopbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stopbtn.Location = new System.Drawing.Point(327, 746);
+            this.stopbtn.Name = "stopbtn";
+            this.stopbtn.Size = new System.Drawing.Size(282, 64);
+            this.stopbtn.TabIndex = 14;
+            this.stopbtn.Text = "Stop";
+            this.stopbtn.UseVisualStyleBackColor = true;
+            this.stopbtn.Click += new System.EventHandler(this.stopbtn_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime.Location = new System.Drawing.Point(539, 366);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 55);
+            this.lblTime.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(373, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 55);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Time :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 873);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.stopbtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.compScore);
+            this.Controls.Add(this.plaScore);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.paperbtn);
+            this.Controls.Add(this.rockbtn);
+            this.Controls.Add(this.scissorsbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scissorsbtn;
+        private System.Windows.Forms.Button rockbtn;
+        private System.Windows.Forms.Button paperbtn;
+        private System.Windows.Forms.Label player;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.Label plaScore;
+        private System.Windows.Forms.Label compScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Timer timGame;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Button stopbtn;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RockPaperScisors/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RockPaperScisors
+{
+    public partial class Form1 : Form
+    {
+
+        public static int startnumb = 4;    //Czas gdzie można wybierać ruch
+        public static int waitnumb = 3;     //Czas gdzie pokazany jest wynik
+        //public static int picknumb = 2;     //Liczba ile wyborów wstecz program ma zapisywać
+        public int numb = startnumb;    //Liczba do wyświetlania czasu
+        public bool game = false;   //Zmienna mówiąca czy gra jest w fazie ruchu czy wyniku
+        public int pick = 0;    //Przechowuje wybór użytkownika
+        public int comppick = 0;    //Przechowuje wybór komputera
+        public int lastpick = 0;    //Ostatnia figura wybrana przez użytkownika
+        public int llastpick = 0;   //Przedostatnia wybrana figura przez użytkownika
+        //public int[] pickTab = new int [picknumb];     //Tablica przechowująca 2 ostatnie wybory użytkownika
+
+        public Form1()
+        {
+            InitializeComponent();
+            timGame.Interval = 1000;
+
+            string path1 = Environment.CurrentDirectory;
+            int cs = path1.Length;
+            int ks = cs - 26;
+            string path2 = path1.Remove(ks);
+            path2 = path2 + "Images/" ;
+            Console.WriteLine(path2);
+
+            Stop();
+            resetBtn.Enabled = false;
+        }
+
+        private void rockbtn_Click(object sender, EventArgs e)
+        {
+            Change("2Rock.png");
+            pick = 1;
+        }
+
+        private void scissorsbtn_Click(object sender, EventArgs e)
+        {
+            Change("Scissors.png");
+            pick = 2;
+        }
+
+        private void paperbtn_Click(object sender, EventArgs e)
+        {
+            Change("1Paper.png");
+            pick = 3;
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            Start();
+            
+        }
+
+        private void stopbtn_Click(object sender, EventArgs e)
+        {
+            Stop();
+            
+        }
+
+        //Funkcja timera sprawdza w jakiej fazie znajduje się gra a następnie zmienia czas
+        private void timGame_Tick(object sender, EventArgs e)
+        {
+            if(game == false)
+            {
+                
+                if (numb < 0)
+                {
+                    game = true;
+
+                    numb = waitnumb;
+
+                    Resault();
+                    BtnEnbFal();
+                }
+
+            }
+            else
+            {
+                
+                if (numb < 0)
+                {
+                    game = false;
+
+                    numb = startnumb;
+
+                    Change("x.png", 2);
+                    BtnEnbTru();
+                }
+            }
+
+            lblTime.Text = Convert.ToString(numb);
+            numb--;
+        }
+
+        //Funkcja przycisku reset 
+        private void Reset()
+        {
+            Start();
+            numb = startnumb;
+            plaScore.Text = "0";
+            compScore.Text = "0";
+
+            pick = 0;
+            lastpick = 0;
+            llastpick = 0;
+
+            Change("x.png", 2);
+
+            game = false;
+
+        }
+
+        //Funkcja zmienia obraz wyboru użytkownika i komputera
+        public void Change(string character, int pr = 1)
+        {
+            string path1 = Environment.CurrentDirectory;
+            int cs = path1.Length;
+            int ks = cs - 26;
+            string path2 = path1.Remove(ks);
+            path2 = path2 + @"Images\" + character;
+
+            if (pr == 1)
+            {
+                picBox1.Image = Image.FromFile(path2);
+            }
+            else if (pr == 2)
+            {
+                picBox1.Image = Image.FromFile(path2);
+                picBox2.Image = Image.FromFile(path2);
+            }
+            else if (pr == 3)
+            {
+                picBox2.Image = Image.FromFile(path2);
+            }
+
+        }
+
+        //Funkcja oceniająca czy użytkonik wybrał jakąś figurę, jeśli nie to pauzuje program
+        public void Resault()
+        {
+            if(pick != 0)
+            {
+                CompTurn();
+                Compare();
+
+            }
+            else
+            {
+                numb = startnumb;
+                game = false;
+                Stop();
+                
+            }
+        }
+
+        //Funkcja porównująca wynik użytkownika i koputera i od tego dodaje punkt
+        private void Compare()
+        {
+            int pkt = Convert.ToInt32(plaScore.Text);
+            int comppkt = Convert.ToInt32(compScore.Text);
+
+            if (pick == 1)
+            {
+                if(comppick == 2)
+                {
+                    pkt++;
+                }
+                else if(comppick == 3)
+                {
+                    comppkt++;
+                }
+            }
+            else if (pick == 2)
+            {
+                if (comppick == 3)
+                {
+                    pkt++;
+                }
+                else if (comppick == 1)
+                {
+                    comppkt++;
+                }
+            }
+            else if (pick == 3)
+            {
+                if (comppick == 1)
+                {
+                    pkt++;
+                }
+                else if (comppick == 2)
+                {
+                    comppkt++;
+                }
+            }
+
+            plaScore.Text = Convert.ToString(pkt);
+            compScore.Text = Convert.ToString(comppkt);
+
+            pick = 0;
+            comppick = 0;
+        }
+
+        //Funkcja odpowiedzialna za ture komputera gdzie wybiera co zrobić na podstawie wcześniejszych wyborów użytkownika
+        public void CompTurn()
+        {
+            if (llastpick == lastpick && llastpick == pick)
+            {
+                switch (pick)
+                {
+                    case 1:
+                        Change("1Paper.png", 3);
+                        comppick = 3;
+                        break;
+                    case 2:
+                        Change("2Rock.png", 3);
+                        comppick = 1;
+                        break;
+                    case 3:
+                        Change("Scissors.png", 3);
+                        comppick = 2;
+                        break;
+                }
+            }
+            else if(lastpick == pick)
+            {
+                llastpick = Pick(lastpick);
+            }
+            else
+            {
+                lastpick = Pick(pick);
+            }
+            
+        }
+
+        //Funkcja odpowiadająca za zwykłe losowanie komputera
+        public int Pick(int choice)
+        {
+            Random r = new Random();
+
+            int l = r.Next(1, 4);
+            comppick = l;
+
+            switch (l)
+            {
+                case 1:
+                    Change("2Rock.png", 3);
+
+                    break;
+                case 2:
+                    Change("Scissors.png", 3);
+                    break;
+                case 3:
+                    Change("1Paper.png", 3);
+                    break;
+            }
+
+            return choice;
+        }
+
+        //Funkcje BtnEnb są odpowiedzialne za zablokowanie i odblokowanie przycisków wybierających figury
+        public void BtnEnbFal()
+        {
+            rockbtn.Enabled = false;
+            scissorsbtn.Enabled = false;
+            paperbtn.Enabled = false;
+        }
+
+        public void BtnEnbTru()
+        {
+            rockbtn.Enabled = true;
+            scissorsbtn.Enabled = true;
+            paperbtn.Enabled = true;
+        }
+
+        //Funkcja zatrzymująca działanie aplikacji
+        public void Stop()
+        {
+            BtnEnbFal();
+
+            startbtn.Enabled = true;
+            stopbtn.Enabled = false;
+
+            timGame.Enabled = false;
+        }
+
+        //Funkcja startująca działanie aplikacji
+        private void Start()
+        {
+
+            if(game == true && numb == waitnumb)
+            {
+                game = false;
+                numb = startnumb;
+            }
+            BtnEnbTru();
+            Change("x.png", 2);
+
+            startbtn.Enabled = false;
+            stopbtn.Enabled = true;
+            resetBtn.Enabled = true;
+
+            timGame.Enabled = true;
+        }
+    }
+}
